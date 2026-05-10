a4c3dafb243e13f4cc7f23ea78707cac2a0cc829
Vortex Digi Labs <contact@vortexdigilabs.com>
2026-05-10 18:32:32 +0200

    feat: implement ImageArchive component with dual auto-scrolling motion galleries

--- src/components/ImageArchive.tsx
+++ src/components/ImageArchive.tsx
@@ -190,14 +190,14 @@ export default function ImageArchive() {
           ref={row1Ref}
           images={ARCHIVE_IMAGES} 
           direction="left" 
-          speed={100} 
+          speed={143} 
           onSelect={setSelectedImage} 
         />
         <ScrollingRow 
           ref={row2Ref}
           images={[...ARCHIVE_IMAGES].reverse()} 
           direction="right" 
-          speed={120} 
+          speed={171} 
           onSelect={setSelectedImage} 
         />
       </div>
